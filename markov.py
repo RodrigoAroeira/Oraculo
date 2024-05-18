b7aa7cb420fd04734258913a17ee8307bf4cc691
RodrigoAroeira <raroeirabr@gmail.com>
2024-05-18 18:06:26 -0300

    Markov.model turned into a property

--- markov.py
+++ markov.py
@@ -6,8 +6,6 @@ class Markov():
         self.file_path = file_path
     
         self.text = self.get_text()
-        # .translate(str.maketrans('','', string.punctuation))
-        self.model = self.model()
         
     def get_text(self):
         text = []
@@ -15,17 +13,16 @@ class Markov():
             text = file.readlines()
         return ' '.join(text)
     
-    def model(self):
+    @property
+    def model(self) -> dict:
         words = self.text.split(' ')
         markov_dict = defaultdict(list)
     
         for current_word, next_word in zip(words[0:-1], words[1:]):
             markov_dict[current_word].append(next_word)
 
-        markov_dict = dict(markov_dict)
-        print(markov_dict)
         # print('Treinado com sucesso!')
-        return markov_dict
+        return dict(markov_dict)
       
 def predict_words(chain: dict, first_word: str, number_of_words: int=5):
     if first_word in list(chain.keys()):
